c8d71c92f978a22791624b355d445075a4ac7973
levanviet859-arch <levanviet859@gmail.com>
2025-12-14 11:29:14 +0700

    Add project files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Quan_li_sinh_vien.UI;
+
+namespace Quan_li_sinh_vien
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new dang_nhap());
+        }
+    }
+}
